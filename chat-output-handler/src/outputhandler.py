d53e4d87382c935588c9120b8d62dd36eec78c18
MitchellHarrison <mitchha22ison@gmail.com>
2021-12-03 16:21:39 -0500

    input and output handler are dataclasses

--- chat-output-handler/src/outputhandler.py
+++ chat-output-handler/src/outputhandler.py
@@ -3,39 +3,25 @@ import os
 import uuid
 import json
 import zmq
-from zmq.asyncio import Context
+import zmq.asyncio
+from dataclasses import dataclass
 
 BACKEND = os.environ.get("BACKEND", "127.0.0.1")
-TWITCH_QUEUE = os.environ.get("TWITCH_QUEUE", "")
-CONTEXT = Context()
 PORT = 5555
 
 # zmq SUB socket address
 CHAT_ADDRESS = f"tcp://{BACKEND}:{PORT}"
-CHAT_SUB_TOPIC = "chat_output"
 
 # zmq PUB socket address
 TWITCH_ADDRESS = f"tcp://0.0.0.0:{PORT}"
 
+@dataclass
 class OutputHandler:
-    def __init__(self, context:Context=CONTEXT, chat_address:str=CHAT_ADDRESS,
-                twitch_address:str=TWITCH_ADDRESS, chat_sub_topic:str=CHAT_SUB_TOPIC,
-                twitch_queue:str=TWITCH_QUEUE):
-        self.context = context
-        self.chat_address = chat_address
-        self.twitch_address = twitch_address
-        self.chat_sub_topic = chat_sub_topic
-        self.twitch_queue = twitch_queue
-
-        # zmq SUB socket
-        self.sub_socket = self.context.socket(zmq.SUB)
-        self.sub_socket.connect(self.chat_address)
-        self.sub_socket.setsockopt(zmq.SUBSCRIBE, bytes(self.chat_sub_topic, "ascii"))
-
-        # zmq PUB socket
-        self.twitch_socket = self.context.socket(zmq.PUB)
-        self.twitch_socket.bind(self.twitch_address)
-
+    context: zmq.asyncio.Context = zmq.asyncio.Context()
+    chat_sub_topic: str = "chat_output"
+    chat_address: str = CHAT_ADDRESS
+    twitch_address: str = TWITCH_ADDRESS
+    twitch_queue: str = os.environ.get("TWITCH_QUEUE", "")
 
     def format_output(self, payload:dict) -> str:
         message = payload["data"]["message"]
@@ -51,7 +37,6 @@ class OutputHandler:
         }
         return json.dumps(output)
         
-
     async def route(self, platform:str, payload:str) -> None:
         print(f"topic = {self.twitch_queue}")
         message = [self.twitch_queue.encode("ascii"), payload.encode("ascii")]
@@ -62,12 +47,19 @@ class OutputHandler:
             else:
                 # this is where another streaming platform output would be
                 pass
-
         except Exception as e:
             print(e)
 
-
     async def run(self) -> None:
+        # zmq SUB socket
+        self.sub_socket = self.context.socket(zmq.SUB)
+        self.sub_socket.connect(self.chat_address)
+        self.sub_socket.setsockopt(zmq.SUBSCRIBE, bytes(self.chat_sub_topic, "ascii"))
+
+        # zmq PUB socket
+        self.twitch_socket = self.context.socket(zmq.PUB)
+        self.twitch_socket.bind(self.twitch_address)
+
         while True:
             # receive message from chat output queue
             _, msg = await self.sub_socket.recv_multipart()
