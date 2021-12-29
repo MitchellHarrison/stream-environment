1fc73fd257f064e3d6db8ee364d6f5ff086f1371
MitchellHarrison <mitchha22ison@gmail.com>
2021-12-29 16:43:46 -0500

    fix color filtering

--- chat-input-handler/src/user.py
+++ chat-input-handler/src/user.py
@@ -1,6 +1,8 @@
 import colorsys
 import random
 
+TWITCH_PURPLE = (145, 70, 255)
+ 
 class TwitchUser:
     def __init__(self, user_id="", display_name="", roles=[], color=""):
         self.user_id = user_id
@@ -10,23 +12,20 @@ class TwitchUser:
         self.is_broadcaster = "broadcaster" in roles
         self.is_mod = "mod" in roles
 
-        # default color to twitch purple
-        default_color = (145, 70, 255)
         # set color if user has chosen a custom color in settings
         if color:
             r,g,b = tuple(int(color[i:i+2], 16) for i in (1,3,5))
             h,s,v = colorsys.rgb_to_hsv(r,g,b)
-            if s < .55 and v < .5:
-                self.color = default_color
-            elif s < .66 and v < .62:
-                self.color = default_color
-            elif s <.78 and v < .73:
-                self.color = default_color
-            elif s < .85 and v < .8:
-                self.color = default_color
-            elif s > .9 and v < .9:
-                self.color = default_color
-            else:
-                self.color = (r,g,b)
+            self.color = (r,g,b)
+            if s < .55 and v < 50:
+                self.color = TWITCH_PURPLE
+            elif s < .66 and v < 62:
+                self.color = TWITCH_PURPLE
+            elif s <.78 and v < 73:
+                self.color = TWITCH_PURPLE
+            elif s < .85 and v < 80:
+                self.color = TWITCH_PURPLE
+            elif s > .9 and v < 90:
+                self.color = TWITCH_PURPLE
         else:
-            self.color = default_color
+            self.color = TWITCH_PURPLE
